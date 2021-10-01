d8638f67a587e6f04c644975685572b09afeb74c
t1dmlgus <dmlgusgngl@gmail.com>
2021-10-01 17:21:39 +0900

    build: 백엔드 프로젝트 생성
    resolved: #1

---
+++ src/main/java/com/s1dmlgus/instagram02/Instagram02Application.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.instagram02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Instagram02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Instagram02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/s1dmlgus/instagram02/Instagram02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.instagram02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Instagram02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
