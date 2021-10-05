b4109879b1d17e330ce34e545b94226d08bedfa1
t1dmlgus <dmlgusgngl@gmail.com>
2021-10-05 22:09:14 +0900

    fix: @EnableJpaAuditing 분리

---
+++ src/main/java/com/s1dmlgus/instagram02/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.s1dmlgus.instagram02.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+}
