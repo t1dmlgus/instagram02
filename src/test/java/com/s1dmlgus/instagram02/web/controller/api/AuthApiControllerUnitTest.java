4421a1c758227c30f9ff94647bb16735c7590774
t1dmlgus <dmlgusgngl@gmail.com>
2021-10-06 17:38:56 +0900

    test: 회원가입 username 유효성검사 예외 처리 테스트
    - GlobalExceptionHandler 사용

--- src/test/java/com/s1dmlgus/instagram02/web/controller/api/AuthApiControllerUnitTest.java
+++ src/test/java/com/s1dmlgus/instagram02/web/controller/api/AuthApiControllerUnitTest.java
@@ -1,6 +1,7 @@
 package com.s1dmlgus.instagram02.web.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.s1dmlgus.instagram02.handler.exception.CustomValidationException;
 import com.s1dmlgus.instagram02.service.UserService;
 import com.s1dmlgus.instagram02.web.dto.ResponseDto;
 import com.s1dmlgus.instagram02.web.dto.auth.JoinDto;
@@ -14,6 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -61,17 +65,19 @@ class AuthApiControllerUnitTest {
     }
 
 
-    @DisplayName("회원가입 유효성 검사 예외 테스트")
+    @DisplayName("회원가입 username 유효성 검사 예외 처리 테스트")
     @Test
-    public void joinDtoExceptionTest() throws Exception {
+    public void joinDtoUsernameExceptionTest() throws Exception {
 
         //given(준비)
         JoinDto joinDto = createExceptionJoinDto();
         String json = new ObjectMapper().writeValueAsString(joinDto);
 
-        // new ResponseDto -> data 가 안담긴다.
-        when(userService.join(joinDto)).thenReturn(new ResponseDto<>("회원가입이 정상적으로 되었습니다.", null));
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("username", "0에서 20자까지 작성할 수 있습니다");
 
+        // new ResponseDto -> data 가 안담긴다.
+        when(userService.join(joinDto)).thenThrow(new CustomValidationException("유효성 검사 실패", errorMap));
 
         //when(테스트)
         ResultActions resultAction = mockMvc.perform(
@@ -85,7 +91,7 @@ class AuthApiControllerUnitTest {
         //then(검증)
         resultAction
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("유효성 검사 실021패"))
+                .andExpect(jsonPath("$.message").value("유효성 검사 실패"))
                 .andExpect(jsonPath("$.data.username").value("0에서 20자까지 작성할 수 있습니다"))
                 .andDo(MockMvcResultHandlers.print());
 
