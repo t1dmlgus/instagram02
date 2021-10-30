b3d35f98b3a45db33a4fc3595398746f6640dfa2
t1dmlgus <dmlgusgngl@gmail.com>
2021-10-31 01:34:55 +0900

    feat: 구독 entity, repository 생성

--- src/main/java/com/s1dmlgus/instagram02/domain/subscribe/Subscribe.java
+++ src/main/java/com/s1dmlgus/instagram02/domain/subscribe/Subscribe.java
@@ -1,6 +1,36 @@
 package com.s1dmlgus.instagram02.domain.subscribe;
 
 import com.s1dmlgus.instagram02.domain.BaseTimeEntity;
+import com.s1dmlgus.instagram02.domain.user.User;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "subscribe_uk",
+                        columnNames = {"to_user_id", "from_user_id"}
+                )
+        }
+)
+@Entity
 public class Subscribe extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User toUser;
+
+    @ManyToOne
+    private User fromUser;
+
+
 }
---
+++ src/main/java/com/s1dmlgus/instagram02/domain/subscribe/SubscribeRepository.java
@@ -0,0 +1,7 @@
+package com.s1dmlgus.instagram02.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+}
