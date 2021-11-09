1e3c7b9381de960b876205b879de75a09973ad7b
t1dmlgus <dmlgusgngl@gmail.com>
2021-11-09 23:12:36 +0900

    feat: 로깅 설정 merge

--- src/main/java/com/s1dmlgus/instagram02/handler/exception/GlobalExceptionHandler.java
+++ src/main/java/com/s1dmlgus/instagram02/handler/exception/GlobalExceptionHandler.java
@@ -2,10 +2,14 @@ package com.s1dmlgus.instagram02.handler.exception;
 
 
 import com.s1dmlgus.instagram02.web.dto.ResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,17 +17,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     // 유효성 검사 예외
     @ExceptionHandler(CustomValidationException.class)
     public ResponseEntity<ResponseDto<?>> validationException(CustomValidationException e) {
 
+        logger.info("validation Exception ! : {} ", e.toString(), e);
         return new ResponseEntity<>(new ResponseDto<>(e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
     }
 
 
     @ExceptionHandler(CustomApiException.class)
-    public ResponseEntity<ResponseDto<?>> Exception(CustomApiException e) {
+    public ResponseEntity<ResponseDto<?>> CustomApiException(CustomApiException e) {
 
+        logger.info("Exception ! : {} ", e.toString(), e);
         return new ResponseEntity<>(new ResponseDto<>(e.getMessage(), null), HttpStatus.BAD_REQUEST);
     }
 
