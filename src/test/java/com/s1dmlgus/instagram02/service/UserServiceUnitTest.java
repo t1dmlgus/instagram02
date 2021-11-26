d3cdb51ebddb50c337553008bcaa06075c15fe9e
t1dmlgus <dmlgusgngl@gmail.com>
2021-11-26 12:32:05 +0900

    test: 프로필 정보 가져오기 단위 테스트

--- src/test/java/com/s1dmlgus/instagram02/service/UserServiceUnitTest.java
+++ src/test/java/com/s1dmlgus/instagram02/service/UserServiceUnitTest.java
@@ -1,11 +1,16 @@
 package com.s1dmlgus.instagram02.service;
 
+import com.s1dmlgus.instagram02.domain.image.Image;
+import com.s1dmlgus.instagram02.domain.image.ImageRepository;
 import com.s1dmlgus.instagram02.domain.user.User;
 import com.s1dmlgus.instagram02.domain.user.UserRepository;
 import com.s1dmlgus.instagram02.handler.exception.CustomApiException;
+import com.s1dmlgus.instagram02.web.controller.UserController;
 import com.s1dmlgus.instagram02.web.dto.ResponseDto;
 import com.s1dmlgus.instagram02.web.dto.auth.JoinRequestDto;
+import com.s1dmlgus.instagram02.web.dto.user.UserProfileResponseDto;
 import com.s1dmlgus.instagram02.web.dto.user.UserUpdateRequestDto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +18,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -28,11 +37,16 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class UserServiceUnitTest {
 
+    Logger logger = LoggerFactory.getLogger(UserServiceUnitTest.class);
+
+
     @InjectMocks
     private UserService userService;
 
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private ImageRepository imageRepository;
 
     @Spy
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -104,6 +118,25 @@ class UserServiceUnitTest {
     }
 
     
+    @DisplayName("프로필정보 가져오기 테스트")
+    @Test
+    public void getProfileTest() throws Exception{
+        //given
+        Long userId = 1L;
+        when(userRepository.findById(any(Long.class))).thenReturn(Optional.ofNullable(createUser()));
+        when(imageRepository.findAllByUserId(any(Long.class))).thenReturn(createImages());
+
+
+        //when
+        UserProfileResponseDto profile = userService.getProfile(userId);
+        logger.info("profile : {}", profile);
+
+        //then
+        Assertions.assertThat(profile.getImages().get(0).getCaption()).isEqualTo("테스트이미지");
+
+    }
+    
+    
     // 회원정보수정 DTO
     private UserUpdateRequestDto createUpdateRequestDto() {
         return UserUpdateRequestDto.builder()
@@ -132,11 +165,26 @@ class UserServiceUnitTest {
     // 유저생성 Entity
     private User createUser() {
         return User.builder()
-                .username("t1dmlgus")
+                .username("test1Dmlgus")
                 .password("1234")
                 .email("dmlgus@gmail.com")
-                .name("이의현")
+                .name("테스트의현")
                 .build();
+    }
+
+
+    private List<Image> createImages(){
+
+        Image testImage = Image.builder()
+                .caption("테스트이미지")
+                .postImageUrl("테스트명.jpg")
+                .user(createUser())
+                .build();
+
+        List<Image> testImages = new ArrayList<>();
+        testImages.add(testImage);
+
+        return testImages;
     }
 
 }
