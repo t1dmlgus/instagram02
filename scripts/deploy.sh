831dfa895e6683febcf975a51a9c58d102c02699
t1dmlgus <dmlgusgngl@gmail.com>
2021-11-29 00:58:23 +0900

    build: 배포자동화 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -26,12 +26,11 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 2)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-
-echo "> JAR NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -40,3 +39,4 @@ echo "> JAR NAME 실행"
 
 nohup java -jar -Dspring.config.location=/home/ec2-user/app/application-dev.yml\
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
