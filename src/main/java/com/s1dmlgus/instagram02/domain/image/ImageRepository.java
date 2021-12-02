ed4e34fc3c8d5833452f49a28a12701d59fde265
t1dmlgus <dmlgusgngl@gmail.com>
2021-12-02 19:28:11 +0900

    feat: 이미지 API(get) 스토리(구독유저 이미지 리스트 가져오기) 구현

--- src/main/java/com/s1dmlgus/instagram02/domain/image/ImageRepository.java
+++ src/main/java/com/s1dmlgus/instagram02/domain/image/ImageRepository.java
@@ -1,11 +1,17 @@
 package com.s1dmlgus.instagram02.domain.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     List<Image> findAllByUserId(Long userId);
+
+    @Query(value = "SELECT * FROM image WHERE user_id IN (SELECT to_user_id FROM subscribe WHERE from_user_id = :principalId)", nativeQuery = true)
+    List<Image> getStory(@Param("principalId") Long principalId);
+
 
 }
--- src/main/java/com/s1dmlgus/instagram02/service/ImageService.java
+++ src/main/java/com/s1dmlgus/instagram02/service/ImageService.java
@@ -6,7 +6,9 @@ import com.s1dmlgus.instagram02.domain.image.Image;
 import com.s1dmlgus.instagram02.domain.image.ImageRepository;
 import com.s1dmlgus.instagram02.handler.exception.CustomApiException;
 import com.s1dmlgus.instagram02.web.dto.ResponseDto;
-import com.s1dmlgus.instagram02.web.dto.image.ImageUploadDto;
+import com.s1dmlgus.instagram02.web.dto.image.upload.ImageUploadRequestDto;
+import com.s1dmlgus.instagram02.web.dto.image.story.ImageStoryResponseDto;
+import com.s1dmlgus.instagram02.web.dto.image.upload.ImageUploadResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -35,51 +38,51 @@ public class ImageService {
 
     // 이미지 업로드
     @Transactional
-    public ResponseDto<?> upload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails){
+    public ResponseDto<?> upload(ImageUploadRequestDto imageUploadRequestDto, PrincipalDetails principalDetails){
 
         // 파일 명
-        String fileName = createFile(imageUploadDto);
+        String fileName = createFile(imageUploadRequestDto);
 
         // 파일업로드
         //uploadFile(imageUploadDto, fileName);
 
         // s3 파일 업로드
-        s3Service.upload(imageUploadDto, fileName);
+        s3Service.upload(imageUploadRequestDto, fileName);
 
 
         // 영속화
-        Image afterUploadImage = imageRepository.save(imageUploadDto.toEntity(fileName, principalDetails.getUser()));
+        Image afterUploadImage = imageRepository.save(imageUploadRequestDto.toEntity(fileName, principalDetails.getUser()));
         logger.info("[after 영속화] : {}", afterUploadImage);
 
-        return new ResponseDto<>("이미지가 업로드 되었습니다.", afterUploadImage.getId());
+        return new ResponseDto<>("이미지가 업로드 되었습니다.", new ImageUploadResponseDto(afterUploadImage));
     }
 
     // 파일명
-    protected String createFile(ImageUploadDto imageUploadDto) {
+    protected String createFile(ImageUploadRequestDto imageUploadRequestDto) {
         
         // 파일 유효성검사
-        validationFile(imageUploadDto);
+        validationFile(imageUploadRequestDto);
         // 파일명
         UUID uuid = UUID.randomUUID();
 
-        return uuid + "_" + imageUploadDto.getFile().getOriginalFilename();
+        return uuid + "_" + imageUploadRequestDto.getFile().getOriginalFilename();
     }
 
     // 유효성 검사
-    protected void validationFile(ImageUploadDto imageUploadDto) {
-        if (imageUploadDto.getFile() == null) {
+    protected void validationFile(ImageUploadRequestDto imageUploadRequestDto) {
+        if (imageUploadRequestDto.getFile() == null) {
             throw new CustomApiException("이미지가 첨부되지 않았습니다.");
         }
     }
 
     // 파일 업로드
-    protected void uploadFile(ImageUploadDto imageUploadDto, String fileName) {
+    protected void uploadFile(ImageUploadRequestDto imageUploadRequestDto, String fileName) {
 
         // 파일경로
         Path imageFilePath = Paths.get(uploadFolder + fileName);
 
         try {
-            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+            Files.write(imageFilePath, imageUploadRequestDto.getFile().getBytes());
 
         } catch (Exception e) {
             throw new CustomApiException("이미지 업로드에 실패했습니다.");
@@ -87,6 +90,13 @@ public class ImageService {
         }
     }
 
+    // 스토리 가져오기
+    @Transactional(readOnly = true)
+    public ResponseDto<?> getStory(PrincipalDetails principalDetails) {
 
+        List<Image> story = imageRepository.getStory(principalDetails.getUser().getId());
 
+        return new ResponseDto<>("이미지 스토리를 가져옵니다.", ImageStoryResponseDto.imageStoryResponseDtoList(story, 0));
+
+    }
 }
